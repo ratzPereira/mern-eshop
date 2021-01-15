ff10f988fe424e27269c361b805d0772af237727
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-15 12:34:51 -0100

    added get product routes to fetch all products and by id from the db.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from '../backend/data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -16,14 +16,7 @@ app.get('/', (req, res) => {
   res.send('Running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 app.listen(PORT, () => {
   console.log('Server running in the port ' + PORT.green.bold)
