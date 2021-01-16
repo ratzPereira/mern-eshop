7b4b388d95ec10924da0c32a18236528d27a8dd3
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-16 18:54:13 -0100

    implemented custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -17,6 +18,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log('Server running in the port ' + PORT.green.bold)
