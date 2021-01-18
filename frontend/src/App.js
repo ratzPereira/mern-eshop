b5db763368a72c797c57234363d298917e0b043d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-18 14:53:56 -0100

    Created the add to cart funtionality and started creating the cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
@@ -14,11 +15,12 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
     </Router>
   )
 }
-
+//the ? in :id is to make the id optional
 export default App
