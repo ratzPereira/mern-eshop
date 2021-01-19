b84b35f13eb1af33797d57f42ce3ee6fc083a926
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-19 15:10:42 -0100

    Added Cart funcionality and impelmented the Cart Screen
    TODO: Work in remove button

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { CART_ADD_ITEM } from '../constants/cartConstrants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`api/products/${id}`)
+  const { data } = await axios.get(`/api/products/${id}`)
 
   dispatch({
     type: CART_ADD_ITEM,
@@ -15,6 +15,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       qty,
     },
   })
+
   //once we dispatch, we want to save it in local storage, so we use the localStorage API.
   //we want to save the entire cart, so we getState and add the cart items. (getState().cart.cartItems) give one JS object so we need to stringify
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,13 +1,13 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstrants'
 
-export const cartReducer = (state = { carItems: [] }, action) => {
+export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload
 
       // search in the state.cartItems array for the existence of the specific product that is defined by item (by the action.payload)
       // if we find a match assign the result to the existItem variable
-      const existItem = state.carItems.find((x) => x.product === item.product)
+      const existItem = state.cartItems.find((x) => x.product === item.product)
       // if there is already a product matching the item in the state.cartItems array
       if (existItem) {
         // return the existing state with adjusted cardItems
@@ -15,7 +15,7 @@ export const cartReducer = (state = { carItems: [] }, action) => {
           ...state,
           // map through the cardItems array and replace the matching product with the new item
           // leave the rest products as they were
-          cartItems: state.carItems.map((x) =>
+          cartItems: state.cartItems.map((x) =>
             x.product === existItem.product ? item : x
           ),
         }
@@ -25,7 +25,7 @@ export const cartReducer = (state = { carItems: [] }, action) => {
         return {
           ...state,
           // return a new cardItems array with the previous products spread and add the new item
-          cartItems: [...state.carItems, item],
+          cartItems: [...state.cartItems, item],
         }
       }
     default:
