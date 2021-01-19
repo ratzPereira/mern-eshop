754e118b406dde67e74c497f08fe26f10b67a446
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-19 19:16:28 -0100

    Added Remove from cart feature

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CART_ADD_ITEM } from '../constants/cartConstrants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstrants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
@@ -18,5 +18,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   //once we dispatch, we want to save it in local storage, so we use the localStorage API.
   //we want to save the entire cart, so we getState and add the cart items. (getState().cart.cartItems) give one JS object so we need to stringify
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  })
+
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
