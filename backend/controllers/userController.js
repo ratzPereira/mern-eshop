ef6bacfe93a4cf3a43bc7ecf354bf741810f5adc
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-25 13:45:13 -0100

    Created user authentication endpoint. auth with email and password working, implementing Tokens next

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+//asyncHandler is a middleware that will deal with errors so we don't need to have try catch in all routes
+// @desc Auth user & get token
+// @route POST /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+})
+
+export { authUser }
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -13,11 +14,14 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.use(express.json()) //allows to accept data in the body
+
 app.get('/', (req, res) => {
   res.send('Running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
