e4877068de6de34b61d69d9e4bab2aa52b4ac6eb
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-25 15:52:45 -0100

    Added user registation and password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,18 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+//before user is save we run this middleware
+userSchema.pre('save', async function (next) {
+  //check if the password is modified, if not we dont want to hash again
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
